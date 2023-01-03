41cbeb34e4e6c6156a00c4ea2113d41997041031
farhanazam41 <39867162+farhanazam41@users.noreply.github.com>
2023-01-02 19:20:34 -0800

    write output file and add unit test

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,62 @@
-const {Shop, Item} = require("../src/gilded_rose");
+const {Shop, Item} = require("../src/gilded_rose.js");
+const tests = require('../tests.json');
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
-  });
+  for( const test of tests ){
+    const [ name, sellIn, quality, outputSellIn, outputQuality ] = test;
+    const description = {
+      name,
+      sellIn,
+      quality,
+      outputSellIn,
+      outputQuality
+    }
+    
+    it(JSON.stringify(description), () => {
+      const gildedRose = new Shop([ new Item(name, sellIn, quality) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).toEqual(outputSellIn);
+      expect(items[0].quality).toEqual(outputQuality);
+    })
+  }
+
+
+// code to generate output in tests.json file
+  // it(
+  //   "should foo", () => {
+  //     const names = [
+  //       "Aged Brie",
+  //       "Backstage passes to a TAFKAL80ETC concert",
+  //       "Sulfuras, Hand of Ragnaros",
+  //       "normal item"
+  //     ]
+    
+  //     const tests = []
+  //     const [ minSellIn, maxSellIn ] = [ -1, 12 ];
+  //     const [ minQuality, maxQuality ] = [ -1, 51 ];
+      
+  //     for(const name of names){
+  //       for(let sellIn = minSellIn; sellIn <= maxSellIn; sellIn++){
+  //         for(let quality = minQuality; quality <= maxQuality; quality++){
+  //          const gildedRose = new Shop([ new Item(name, sellIn, quality) ]);
+  //           const items = gildedRose.updateQuality();
+    
+  //           const outputSellIn = items[0].sellIn;
+  //           const outputQuality = items[0].quality;
+    
+  //           tests.push([
+  //             name,
+  //             sellIn,
+  //             quality,
+  //             outputSellIn,
+  //             outputQuality
+  //           ])
+  //         }
+  //       }
+  //     }
+    
+  //     console.log(JSON.stringify(tests))
+  //     console.log(tests.length)
+  //   }
+  // )
 });
